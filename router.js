43a4a2077eac1f19e23309fb303d7bc971610214
Daniel Beecham <daniel@lunix.se>
2013-02-21 13:33:26 +0100

    Dispatch -> Router. Signal handling.
    --HG--
    rename : dispatch.json => routes.json

--- router.js
+++ router.js
@@ -5,7 +5,7 @@ module.exports = (function () {
 
     // closure
     var url = require('url'),
-        handlers = require('./dispatch.json'),
+        handlers = require('./routes.json'),
         handler;
 
     return function (uri) { // TODO: ugly names
@@ -13,10 +13,12 @@ module.exports = (function () {
         // TODO: regexp this
         handler = handlers[url.parse(uri).pathname];
         if (handler) {
+            process.stdout.write('found.\n');
             return require('./handlers/' + handlers[url.parse(uri).pathname] + '.js');
         }
 
         return function (request, response) { // Handle this better.
+            process.stdout.write('404.\n');
             response.writeHead(404, {'Content-Type': 'text/plain'});
             response.end('404!\n');
         };
